baa2425762b1407f44dc0b4ac02eee7dabb9e069
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2024-07-12 19:30:42 +0500

    Add files via upload

---
+++ some,every,find,findindex/app.js
@@ -0,0 +1,224 @@
+
+let a = [4, 6, 67, 87, 9, 6, 8, 89];
+
+
+
+// let greaterFifty = a.filter((value, index, arr) => {
+//     return value > 50;
+// });
+// console.log(greaterFifty);
+
+
+// a.filter((value) => value > 50);
+
+// abc();
+
+// function abc() {
+//     console.log("hello");
+// }
+
+
+// const abc = () => {
+//     console.log("hello");
+// }
+// abc();
+
+
+// const sum = (a) => a % 2 === 0 ? console.log("even") : console.log("odd");
+// sum(4545);
+
+
+
+
+// let filterd = a.map((item) => {
+//     if (item > 5) {
+//         return item;
+//     }
+// })
+// console.log(filterd[0]);
+
+
+
+
+
+
+// Filter more than 2
+// Find  only for one
+
+
+let obj = [
+    {
+        name: "sadiq",
+        age: 26,
+    },
+    {
+        name: "ali",
+        age: 34,
+    },
+    {
+        name: "sadiq",
+        age: 36,
+    }
+]
+
+// let abc = obj.filter((item) => {
+//     return item.name === "sadiq";
+// })
+// console.log(abc);
+
+// let findData = obj.find((item) => {
+//     return item.name === "sadiq";
+// })
+// console.log(findData)
+
+
+
+// let data = [4, 56, 567, 43, 67, 34, 8, 43, 76];
+
+// let returnData = data.filter((item) => {
+//     return item > 10;
+// })
+// console.log(returnData);
+
+
+// let returnDataViaFind = data.find((item) => {
+//     return item > 10;
+// })
+// console.log(returnDataViaFind);
+
+
+
+
+// let userInfo = [
+//     {
+//         name: "sadiq",
+//         email: "sadiq@gmail.com"
+//     },
+//     {
+//         name: "sami",
+//         email: "sami@gmail.com"
+//     },
+//     {
+//         name: "ali",
+//         email: "ali@gmail.com"
+//     },
+
+// ]
+// let email = prompt("Enter a email")
+// let existingEmail = userInfo.find((item) => {
+//     return item.email === email;
+// })
+// console.log(existingEmail);
+
+// if (existingEmail) {
+//     alert("email already exisit")
+// }
+// else {
+//     alert("data stored")
+// }
+
+
+// some and every 
+
+// let data = [4, 5, 4, 10];
+
+// let someData = data.some((item) => {
+//     return item > 5;
+// })
+// console.log(someData)
+
+// let everyData = data.every((item) => {
+//     return item < 4;
+// })
+// console.log(everyData);
+
+
+
+// swap two numbers without create any variable 
+
+// const swapTwoNumber = (a, b) => {
+
+//     // code here 
+// }
+// swapTwoNumber(2, 3);
+
+
+// let x = 10;
+// let y = 20;
+
+// x = x + y;  //(30)
+// y = x - y;   //10 
+// x = x - y;  //30 - 10 = 20
+
+
+const [x, y] = [y, x]
+
+
+
+let data = JSON.parse(localStorage.getItem('data')) || [];
+
+
+
+// let filterd = data.find((item) => {
+//     return item.email === signupEmail.value;
+// })
+// if (filterd) {
+//     alert("ALREAD EXISIT");
+// }
+// else {
+//     //  push
+// }
+// [
+//     { sadiqshah@gmail.com
+// 12345}
+// ]
+
+// data.find((item=>{
+//     return item.email=== loginEmail.value;
+// }))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
