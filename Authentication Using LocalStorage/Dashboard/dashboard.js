56938360f3eef921e677c1e478080de30f5e5715
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2024-07-15 00:37:19 +0500

    Add files via upload

--- Authentication Using LocalStorage/Dashboard/dashboard.js
+++ Authentication Using LocalStorage/Dashboard/dashboard.js
@@ -3,15 +3,22 @@ let ImageProfile = document.getElementById('img');
 const data = JSON.parse(localStorage.getItem("userData"));
 
 
+// hum ny find login ek common bna lia hai
+let findLogin = data.find((item) => {
+    return item.isLogin === true;
+});
+
+
+// agr logout ho chuka hai phir b dashboard par jana chye
+// to hm usy wapis login par redirect kr rhy hain 
+const redirection = () => {
+    if (!findLogin) {
+        window.location.href = '../login/login.html'
+    }
+}
+redirection();
 
 const showData = () => {
-
-    // yhn hm wo data find krngy jo login hoga 
-    let findLogin = data.find((item) => {
-        return item.isLogin === true;
-    })
-
-
     const { user, userProfile } = findLogin;
     Username.innerText = user;
     ImageProfile.src = userProfile;
@@ -19,17 +26,16 @@ const showData = () => {
 showData();
 
 
+
 const logout = () => {
     // yhn apko isLogin ko false kr k wapis singup par bjh dyna hai 
-    let findLogin = data.find((item) => {
-        return item.isLogin === true;
-    });
     if (findLogin) {
         findLogin.isLogin = false;
 
+
         // update localStorage
         localStorage.setItem('userData', JSON.stringify(data))
-        window.location.href = '../signup/index.html'
+        window.location.href = '../login/login.html'
     }
 
 }
--- Authentication Using LocalStorage/Login/login.js
+++ Authentication Using LocalStorage/Login/login.js
@@ -5,46 +5,62 @@ const [loginEmail, loginPassword] = formFeild;
 
 const data = JSON.parse(localStorage.getItem("userData"));
 
-function isLoginAlready() {
-    let isLoginHai = data.find((item) => {
-        return item.isLogin === true;
-    })
-    if (isLoginHai) {
-        window.location.href = "../Dashboard/dashboard.html"
-    }
-}
-isLoginAlready();
 
+// agr local storage first time main khali ho or koi 
+// redirect krna chaye to.
+if (!data === null) {
+    function isLoginAlready() {
+        let isLoginHai = data.find((item) => {
+            return item.isLogin === true;
+        })
+        if (isLoginHai) {
+            window.location.href = "../Dashboard/dashboard.html"
+        }
+    }
+    isLoginAlready();
+}
 
 let loader = document.getElementById('loader');
+console.log(data)
 const login = () => {
     event.preventDefault();
 
-    let findLogin = data.find((item) => {
-        return item.signUpEmail === loginEmail.value;
-    })
-    if (findLogin) {
-        findLogin.isLogin = true;
-        localStorage.setItem("userData", JSON.stringify(data));
-        if (findLogin.signUpEmail === "admin@admin.com") {
-            loader.classList.add('loader');
-            setTimeout(() => {
-                window.location.href = '../Dashboard/dashboard.html'
-                loader.classList.remove('loader');
-            }, 2000)
+
+    // agr local storage first time main khali ho or koi 
+    // login krna cha rha ho to 
+    if (data !== null) {
+        let findLogin = data.find((item) => {
+            return item.signUpEmail === loginEmail.value;
+        })
+        if (findLogin) {
+            findLogin.isLogin = true;
+            localStorage.setItem("userData", JSON.stringify(data));
+            if (findLogin.signUpEmail === "admin@admin.com") {
+                loader.classList.add('loader');
+                setTimeout(() => {
+                    window.location.href = '../Dashboard/Admin.html'
+                    loader.classList.remove('loader');
+                }, 2000)
+            }
+            else if (findLogin.signUpEmail === loginEmail.value) {
+                loader.classList.add('loader');
+                setTimeout(() => {
+                    window.location.href = '../Dashboard/dashboard.html'
+                    loader.classList.remove('loader');
+                }, 2000)
+            }
         }
-        else if (findLogin.signUpEmail === loginEmail.value) {
-            loader.classList.add('loader');
-            setTimeout(() => {
-                window.location.href = '../Dashboard/dashboard.html'
-                loader.classList.remove('loader');
-            }, 2000)
+
+        else {
+            alert("bhai account bna ly phle...")
         }
+    }
+    else {
+        alert("Database is not available");
     }
 
-    else {
-        alert("bhai account bna ly phle...")
-    }
+
+
 
     // if ((signUpEmail === loginEmail.value) &&
     //     signupPassword === loginPassword.value) {
